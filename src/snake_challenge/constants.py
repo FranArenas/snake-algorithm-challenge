628d6cc60b495519226a39010a6d46602f3f448c
Fran Arenas <franarenasafan@gmail.com>
2022-03-17 21:39:20 +0100

    Add numeric constants

--- src/snake_challenge/constants.py
+++ src/snake_challenge/constants.py
@@ -1,5 +1,17 @@
 import enum
 
+# Snake constants
+SNAKE_MIN_LENGTH = 3
+SNAKE_MAX_LENGTH = 7
+
+# Board constants
+BOARD_MAX_X = BOARD_MAX_Y = 10
+BOARD_MIN_X = BOARD_MIN_Y = 1
+
+# Depth constants
+MIN_DEPTH = 1
+MAX_DEPTH = 20
+
 
 class SnakeMovement(enum.Enum):
     """
